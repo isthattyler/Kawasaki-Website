02645ea4e0f030083d840e4f92f2ba9203879f0a
Jeff Shi <jeffshi717@gmail.com>
2019-08-24 08:40:34 -0400

    attempted implementation of online ordering and added comments

--- menu.js
+++ menu.js
@@ -20,7 +20,61 @@ function closeModal() {
 /* close the modal that is currently open by clicking on any part of the window that's not the modal itself
 (variable for the open modal is given from the openModal function) */
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == window.modal) {
     window.modal.style.display = "none";
   }
+}
+
+
+/* Shopping Cart */
+/* checks whether the HTML document has been loaded in*/
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+/* when the document is ready, we will start to find all of the add to cart
+buttons for every menu item and listen for when they are clicked on*/
+function ready() {
+  var addToCartButtons = document.getElementsByClassName('cart-button');
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", addToCartClicked);
+  }
+}
+
+/* when add to cart button is clicked, we want to add the item to cart
+by running a separate function and sending the item name (title) and 
+the price of the item to the other function as well*/
+function addToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement.parentElement.parentElement.parentElement;
+  console.log(shopItem)
+  var title = shopItem.getElementsByClassName('itemName')[0].innerText;
+  var price = shopItem.getElementsByClassName('price')[0].innerText;
+  addItemToCart(title, price);
+}
+
+/* function to create a new div to show the items that have been added to cart
+along with their name and price*/
+function addItemToCart(title, price) {
+  var cartRow = document.createElement('div');
+  var cartItems = document.getElementsByClassName('cart-items')[0];
+
+  var cartRowContents =
+  `
+      <div class="cart-item cart-column">
+        <span class="cart-itemName"> ${title} </span>
+      </div>
+
+      <span class="cart-price cart-column"> ${price} </span>
+
+      <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button>" REMOVE </button>
+      </div>
+  `
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
 }
