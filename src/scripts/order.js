9d409996e198851a5772bc28f425c7a6e081fade
Tyler T. Nguyen <tyler.t.nguyen@uconn.edu>
2019-08-25 15:17:37 -0400

    Added confirmed order

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var numOfOrders = 0;
+	var numOfOrders = $("#listOfOrders").children().length;
 	$(".num").text(numOfOrders);
 
 	// hide dialogs on start
@@ -8,5 +8,42 @@ $(document).ready(function() {
 	$('input[type="checkbox"]').click(function(){
         var inputValue = document.getElementById("shopping-cart")
         $(inputValue).toggle();
-    });
+	});
+
+	// Finish order
+	$(".checkout").on("click", function () {
+		$("#finalOrderList > ol").children().remove();
+		   $(".orderName").children("span").each(function(){
+			   var finalOrder = '<li>' + $(this).text() + '</li>';
+			   $("#finalOrderList > ol").append(finalOrder);
+		   })
+   
+		   $("#finishOrderDialog").dialog({
+			   hide: "blind",
+			   show : "blind",
+			   width: "500px",
+		   	   closeText: "X"
+		   });
+	   })
+   
+	   $(".order").on("click", function () {
+		   var name = $("#buyerName").val();
+		   var number = $("#buyerNumber").val();
+		   var address = $("#buyerAddress").val();
+		   $("#buyerInfo").children("p").remove();
+   
+		   if (name != "" && number != "" && address != "") {
+			   $("#finishOrderDialog").dialog("close");
+			   $("#thanksMessage").dialog({
+				   hide: "blind",
+				   show : "blind",
+				   width: "400px"
+			   });
+			   setTimeout(function(){
+				   $("#thanksMessage").dialog("close");
+			   }, 3000);
+		   }else{
+			   $("#buyerInfo").append('<p>Please fill all the fields.</p>');
+		   }
+	   })
 });
